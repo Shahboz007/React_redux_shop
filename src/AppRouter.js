77136c0888efe48c6088899e61cd1075b9550641
shahboz007 <olmasovshahboz007@gmail.com>
2022-09-17 01:32:54 -0700

    create all router control file

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,15 +1,20 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { publicRoutes } from "./routes";
+import Navbar from "./components/Navbar";
 
 function AppRouter() {
-  const user = false;
   return (
-    <Routes>
-      {publicRoutes.map((item, i) => (
-        <Route path={item.path} element={item.element} key={i} />
-      ))}
-    </Routes>
+    <>
+      <Navbar />
+      <Routes>
+        {/* {publicRoutes.map((item, i) => (
+          <Route path={item.path} element={item.element} key={i} />
+        ))} */}
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/test" element={<h1>Home</h1>} />
+      </Routes>
+    </>
   );
 }
 
